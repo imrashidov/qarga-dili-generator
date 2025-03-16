c5a3dcf3eb9200fa3b89e85ff3438bdd23589e79
imrashidov <farikrashidov@gmail.com>
2025-03-16 18:13:35 +0400

    Add: Sitemap generator

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -2,12 +2,4 @@
 module.exports = {
   siteUrl: process.env.SITE_URL || "https://qargadili.vercel.app",
   generateRobotsTxt: true,
-  generateIndexSitemap: true,
-  robotsTxtOptions: {
-    additionalSitemaps: [
-      `${process.env.SITE_URL || "https://qargadili.vercel.app"}/sitemap.xml`,
-    ],
-  },
-  exclude: ["/admin", "/login", "/private*"],
-  // For additional configuration options, see https://github.com/iamvishnusankar/next-sitemap
 };
